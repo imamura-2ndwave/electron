8b32942229c283b0c39f4d027464d609f136726a
yuki <imamura@2ndwave.jp>
2017-07-15 11:31:40 +0900

    npm init -y; npm install electron --save-dev

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// index.js (main process)
+// - GUI (renderer process)
+// - GUI (renderer process)
+// - GUI (renderer process)
+
+const electron = require('electron');
+const app = electron.app;
+
+// アプリの初期化が終わった時に呼ばれるイベント
+app.on('ready', function() {
+    // create window
+});
