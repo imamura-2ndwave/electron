ec05d07e8e9da8a14e0912e32562dc09cb336a41
yuki <imamura@2ndwave.jp>
2017-07-17 21:20:49 +0900

    BrowserWindowの作成

--- index.js
+++ index.js
@@ -7,8 +7,18 @@
 
 const electron = require('electron');
 const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
 
 // アプリの初期化が終わった時に呼ばれるイベント
 app.on('ready', function() {
     // create window
+    mainWindow = new BrowserWindow({ width: 600, height: 400});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.webContents.openDevTools();  // debug用
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
